3529d499f3bd5ecff172c185d151753b7d0f0684
Vlad-Stefan Sas <me@svs.pm>
2025-01-03 23:20:46 +0200

    fix: add categories if they don't exist in db

--- backend/src/categories/categories.service.ts
+++ backend/src/categories/categories.service.ts
@@ -3,12 +3,12 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Category, CategoryDocument } from './categories.schema';
 import { CreateCategoryDto } from './dto/create-category.dto';
-import DefaultCategories from './categories.default.json'
+import { readFile } from 'fs/promises';
 
 @Injectable()
 export class CategoriesService {
     constructor(@InjectModel(Category.name) private categoryModel: Model<CategoryDocument>) {
-        this.initializeIfEmpty(DefaultCategories)
+        this.initializeIfEmpty();
     }
 
     async create(createCategoryDto: CreateCategoryDto): Promise<Category> {
@@ -16,16 +16,27 @@ export class CategoriesService {
         return newCategory.save();
     }
 
-    async initializeIfEmpty(values) {
-    try {
-        for(let i = 0; i < values.length; i ++)
-        {
-            const result = await this.categoryModel.updateOne(new CreateCategoryDto(values[i].name, values[i].id_icon, values[i].id_user, values[i].color), { upsert: true });
-            console.log('Operation result:', result);
-        }
-    } catch (error) {
-        console.error('Error inserting document:', error);
-    }
-}
+    async initializeIfEmpty() {
+        try {
+            // Path should be relative to the root of the project
+            // If not JSON, the nestjs cli config will have to be configured to add that file type
+            const data = await readFile('src/categories/categories.default.json', 'utf8');
+            const jsonData: CreateCategoryDto[] = JSON.parse(data);
+            for (const category of jsonData) {
+                const result = await this.categoryModel.updateOne(
+                    { name: category.name, id_user: category.id_user },
+                    { $setOnInsert: category },
+                    { upsert: true }
+                );
 
+                if (result.upsertedCount > 0) {
+                    console.log('Category inserted:', category.name);
+                } else {
+                    console.log('Category already exists:', category.name);
+                }
+            }
+        } catch (error) {
+            console.error('Error inserting document:', error);
+        }
+    }
 }
