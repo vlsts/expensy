c04089c3e38b4554b8a0fe1ec56f98b2131990c0
Vlad-Stefan Sas <me@vlad.pm>
2025-01-04 19:00:58 +0200

    feat: deploy step, cloudinit file and removed compose

--- terraform/modules/vm/main.tf
+++ terraform/modules/vm/main.tf
@@ -7,16 +7,20 @@ resource "digitalocean_droplet" "frontend" {
   image    = "ubuntu-24-10-x64"
   name     = "frontend"
   region   = "fra1"
-  size     = "s-1vcpu-1gb"
+  size     = "s-1vcpu-2gb-amd"
   ssh_keys = [digitalocean_ssh_key.ssh_key.fingerprint]
   ipv6     = true
+
+  user_data = file("./modules/vm/cloudinit.yml")
 }
 
 resource "digitalocean_droplet" "backend" {
   image    = "ubuntu-24-10-x64"
   name     = "backend"
   region   = "fra1"
-  size     = "s-1vcpu-1gb"
+  size     = "s-1vcpu-2gb-amd"
   ssh_keys = [digitalocean_ssh_key.ssh_key.fingerprint]
   ipv6     = true
+
+  user_data = file("./modules/vm/cloudinit.yml")
 }
