26db61998b12e27dc04c2a03b79a8075732213a5
Vlad-Stefan Sas <me@vlad.pm>
2025-01-05 23:18:49 +0200

    feat: swagger page

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const configService: ConfigService = app.get(ConfigService);
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+    const config = new DocumentBuilder()
+        .setTitle('Expensy Backend')
+        .setDescription('The Expensy API description')
+        .setVersion('1.0')
+        .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    
+    SwaggerModule.setup('', app, documentFactory);
+    
     await app.listen(configService.get<number>('port'));
 }
 
