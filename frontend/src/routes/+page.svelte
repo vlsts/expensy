7859c62bd53bb412d49e579102400732c2bbdcc8
Vlad-Stefan Sas <me@vlad.pm>
2025-01-06 02:08:55 +0200

    feat: initial frontend test corbado

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { goto } from '$app/navigation';
+	import Corbado from '@corbado/web-js';
+	import { onMount } from 'svelte';
+ 
+	onMount(async () => {
+	});
+</script>
+
+
+<h1>basic page no auth</h1>
