048f5eefdaa5ceb7380419f5d2a8f274f99e1357
Stoica-David <stoicadavidioan@gmail.com>
2025-01-12 05:09:25 +0200

    Enabled cors on backend.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors();
     const configService: ConfigService = app.get(ConfigService);
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
