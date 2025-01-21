7242ed0423b12e74571400376e43ba661245f665
Vlad-Stefan Sas <me@vlad.pm>
2025-01-21 19:36:53 +0200

    fix: cors config and new var name in workflow

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -11,7 +12,19 @@ async function bootstrap() {
     app.enableCors({
         origin: configService.get<string>('FRONTEND_URL'),
         credentials: true,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        allowedHeaders: [
+            'Origin',
+            'X-Requested-With',
+            'Content-Type',
+            'Accept',
+            'Authorization',
+        ],
+        preflightContinue: false,
+        optionsSuccessStatus: 204
     });
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ extended: true, limit: '50mb' }));
     app.use(cookieParser())
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
